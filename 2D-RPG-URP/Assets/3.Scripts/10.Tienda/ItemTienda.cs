0bc7afd72596958ab9c43dcadaee170cae72643a
GustavoMendozaAren <stavmdza@gmail.com>
2024-11-21 06:17:53 -0600

    ItemsCreadosYComprarItemsEnTienda

--- 2D-RPG-URP/Assets/3.Scripts/10.Tienda/ItemTienda.cs
+++ 2D-RPG-URP/Assets/3.Scripts/10.Tienda/ItemTienda.cs
@@ -12,7 +12,17 @@ public class ItemTienda : MonoBehaviour
     [SerializeField] private TextMeshProUGUI itemCosto;
     [SerializeField] private TextMeshProUGUI cantidadPorComprar;
 
-    public ItemVenta ItemCargado { get; set; }
+    public ItemVenta ItemCargado { get; private set; }
+
+    private int cantidad;
+    private int costoInicial;
+    private int costoActual;
+
+    private void Update()
+    {
+        cantidadPorComprar.text = cantidad.ToString();
+        itemCosto.text = costoActual.ToString();
+    }
 
     public void ConfigurarItemEnVenta(ItemVenta itemVenta)
     {
@@ -20,6 +30,40 @@ public class ItemTienda : MonoBehaviour
         itemIcono.sprite = itemVenta.item.Icono;
         itemNombre.text = itemVenta.item.Nombre;
         itemCosto.text = itemVenta.Costo.ToString();
+        cantidad = 1;
+        costoInicial = itemVenta.Costo;
+        costoActual = itemVenta.Costo;
+    }
 
+    public void ComprarItem()
+    {
+        if (MonedasManager.Instance.MonedasTotales >= costoActual)
+        {
+            Inventario.Instance.AniadirItem(ItemCargado.item, cantidad);
+            MonedasManager.Instance.RemoverMonedas(costoActual);
+            cantidad = 1;
+            costoActual = costoInicial;
+        }
+    }
+
+    public void SumarItemPorComprar()
+    {
+        int costoDeCompra = costoInicial * (cantidad + 1);
+        if (MonedasManager.Instance.MonedasTotales >= costoDeCompra)
+        {
+            cantidad++;
+            costoActual = costoInicial * cantidad;
+        }
+    }
+
+    public void RestarItemPorComprar()
+    {
+        if (cantidad == 1)
+        {
+            return;
+        }
+
+        cantidad--;
+        costoActual = costoInicial * cantidad;
     }
 }
